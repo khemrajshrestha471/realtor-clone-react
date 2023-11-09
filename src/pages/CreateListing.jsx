3cb5b917b73df0f8fe52b5872a53f961d53e9c99
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-11-09 18:57:02 +0545

    pending from here

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
+import Spinner from '../components/Spinner';
+import { toast } from "react-toastify";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import { serverTimestamp } from "firebase/firestore";
+
 
 const CreateListing = () => {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const auth = getAuth();
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -13,13 +28,98 @@ const CreateListing = () => {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
+    images: {},
   });
-  const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice } = formData;
-  function onChange() {}
+  const { type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, latitude, longitude, images } = formData;
+  function onChange(e) {
+    let boolean = null;
+    if(e.target.value === "true") {
+      boolean = true;
+    }
+    if(e.target.value === "false") {
+      boolean = false;
+    }
+    if(e.target.files) {
+      setFormData((prevState)=>({
+        ...prevState,
+        images: e.target.files
+      }));
+    }
+    if(!e.target.files) {
+      setFormData((prevState)=>({
+        ...prevState,
+        [e.target.id] : boolean ?? e.target.value, 
+      }));
+    }
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    if(discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price should be less than Regular price!");
+      return;
+    }
+    if(images.length > 6) {
+      setLoading(false);
+      toast.error("Maximum 6 images are allowed!");
+      return;
+    }
+    async function storeImage(image) {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_change",
+          (snapshot) => {
+            const progress = 
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    }
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      timestamp: serverTimestamp(),
+    };
+    
+  }
+  if(loading) {
+    return <Spinner />
+  }
   return (
     <main className="max-w-md px-2 mx-auto ">
       <h1 className="text-3xl text-center mt-6 font-bold">Create a Listing</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <p className="text-lg mt-6 font-semibold">Sell / Rent</p>
         <div className="flex">
           <button
@@ -38,7 +138,7 @@ const CreateListing = () => {
           <button
             type="button"
             id="type"
-            value="sale"
+            value="rent"
             onClick={onChange}
             className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${
               type === "sale"
@@ -157,6 +257,18 @@ const CreateListing = () => {
           required
           className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
         />
+        {!geolocationEnabled && (
+          <div className="flex space-x-6 justify-start mb-6 ">
+            <div>
+              <p className="text-lg font-semibold">Latitude</p>
+              <input type="number" id="latitude" value={latitude} onChange={onChange} required min="-90" max="90" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"/>
+            </div>
+            <div>
+              <p className="text-lg font-semibold">Longitude</p>
+              <input type="number" id="longitude" value={longitude} onChange={onChange} required min="-180" max="180"  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"/>
+            </div>
+          </div>
+        )} 
         <p className="text-lg font-semibold">Description</p>
         <textarea
           type="text"
